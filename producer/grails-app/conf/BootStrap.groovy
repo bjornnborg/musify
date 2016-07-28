eae824e7018fde0d6294bc075c2c6887a363d43f
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-07-28 17:26:50 -0300

    registering json converter for songs

--- producer/grails-app/conf/BootStrap.groovy
+++ producer/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import grails.converters.JSON
+import meli.musify.canonic.Song
+
 class BootStrap {
 
     def init = { servletContext ->
+        JSON.registerObjectMarshaller(Song){Song s ->
+            def props = ["id": s.id] << s.properties
+            return props.findAll {k,v -> k != 'class'}
+        }
     }
     def destroy = {
     }
