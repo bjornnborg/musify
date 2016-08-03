7b94d87844cd1b1fa59f9f11fc56fb75bbf2b606
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-03 15:50:00 -0300

    starting consumers on application startup

--- consumer/grails-app/conf/BootStrap.groovy
+++ consumer/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,21 @@
+import meli.musify.consumer.BigQueueConsumer
+
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+        println "XXXXXXXXXXXXXXXXX"
+        println "Bootstrap do consumidor"
+        println "XXXXXXXXXXXXXXXXX"
+        grailsApplication.getMainContext().getBeanDefinitionNames().findAll{it.toLowerCase().contains("consumer")}.each {
+            println "Achei um bean $it"
+        }
+        grailsApplication.getMainContext().getBeansOfType(BigQueueConsumer).each { beanName, bean ->
+            println "************************"
+            println "vamos fazer o start dos beans"
+            bean.start()
+        }
     }
     def destroy = {
     }
