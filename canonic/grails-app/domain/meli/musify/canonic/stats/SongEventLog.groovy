d72ff9215549b8e40e3a48ed5049a91e5f5bd5a9
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-03 17:38:47 -0300

    creating model for song events log

---
+++ canonic/grails-app/domain/meli/musify/canonic/stats/SongEventLog.groovy
@@ -0,0 +1,14 @@
+package meli.musify.canonic.stats
+
+import meli.musify.canonic.Song
+
+class SongEventLog {
+
+    String eventType
+    Date dateCreated
+    Long songId
+
+    static constraints = {
+    }
+
+}
