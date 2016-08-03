1e4b0c391771364636e33bed1164a64d28fd57af
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-03 17:40:55 -0300

    creating song event log after process messages

--- consumer/grails-app/services/meli/musify/consumer/SongsEventsConsumerService.groovy
+++ consumer/grails-app/services/meli/musify/consumer/SongsEventsConsumerService.groovy
@@ -1,7 +1,9 @@
 package meli.musify.consumer
 
-import grails.converters.JSON
+import grails.transaction.Transactional
+import meli.musify.canonic.Song
 import meli.musify.canonic.command.PlayerCommand
+import meli.musify.canonic.stats.SongEventLog
 import net.sf.json.JSONObject
 
 class SongsEventsConsumerService implements BigQueueConsumer {
@@ -10,12 +12,17 @@ class SongsEventsConsumerService implements BigQueueConsumer {
     String consumerName = "song_events_consumer"
 
     @Override
+    @Transactional
     void onDelivery(Object message) {
         println "============"
         println "Ah, muleque!!!! Mensagem entregue ao consumidor!!!!"
         println message
-        def parsed = JSONObject.toBean(message.msg, PlayerCommand)
-        println parsed
+        //def parsed = JSONObject.toBean(message.msg, PlayerCommand)
+
+        def songLog = new SongEventLog(eventType: message.msg.commandType, songId: message.msg.songId)
+        songLog.save()
+
+        //println parsed
         println "============"
     }
 }
