d00f41e7beee778a32f471b553647fd9c4bc7519
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-04 14:33:55 -0300

    elastic settings

--- producer/grails-app/conf/Config.groovy
+++ producer/grails-app/conf/Config.groovy
@@ -2,6 +2,8 @@
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
 
+
+grails.config.locations = [ ElasticSearchConfig ]
 // grails.config.locations = [ "classpath:${appName}-config.properties",
 //                             "classpath:${appName}-config.groovy",
 //                             "file:${userHome}/.grails/${appName}-config.properties",
