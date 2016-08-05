620965e9ac908608f4900905337f63895f76d578
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-05 16:15:24 -0300

    code cleanup

--- consumer/grails-app/conf/BootStrap.groovy
+++ consumer/grails-app/conf/BootStrap.groovy
@@ -5,15 +5,7 @@ class BootStrap {
     def grailsApplication
 
     def init = { servletContext ->
-        println "XXXXXXXXXXXXXXXXX"
-        println "Bootstrap do consumidor"
-        println "XXXXXXXXXXXXXXXXX"
-        grailsApplication.getMainContext().getBeanDefinitionNames().findAll{it.toLowerCase().contains("consumer")}.each {
-            println "Achei um bean $it"
-        }
         grailsApplication.getMainContext().getBeansOfType(BigQueueConsumer).each { beanName, bean ->
-            println "************************"
-            println "vamos fazer o start dos beans"
             bean.start()
         }
     }
--- consumer/grails-app/services/meli/musify/consumer/SongsEventsConsumerService.groovy
+++ consumer/grails-app/services/meli/musify/consumer/SongsEventsConsumerService.groovy
@@ -1,10 +1,7 @@
 package meli.musify.consumer
 
 import grails.transaction.Transactional
-import meli.musify.canonic.Song
-import meli.musify.canonic.command.PlayerCommand
 import meli.musify.canonic.stats.SongEventLog
-import net.sf.json.JSONObject
 
 class SongsEventsConsumerService implements BigQueueConsumer {
 
@@ -14,15 +11,9 @@ class SongsEventsConsumerService implements BigQueueConsumer {
     @Override
     @Transactional
     void onDelivery(Object message) {
-        println "============"
-        println "Ah, muleque!!!! Mensagem entregue ao consumidor!!!!"
         println message
-        //def parsed = JSONObject.toBean(message.msg, PlayerCommand)
 
         def songLog = new SongEventLog(eventType: message.msg.commandType, songId: message.msg.songId)
         songLog.save()
-
-        //println parsed
-        println "============"
     }
 }
