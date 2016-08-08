3ffedd6c1179198fef423f6592f363acc85efe58
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-08 14:02:01 -0300

    adding property to know at which second a 'listening' event was issued

--- canonic/src/groovy/meli/musify/canonic/command/PlayerCommand.groovy
+++ canonic/src/groovy/meli/musify/canonic/command/PlayerCommand.groovy
@@ -9,9 +9,11 @@ class PlayerCommand {
     Long userId
     Long songId
     Long playlistId
+    Long atSecond
 
     static constraints ={
         playlistId nullable: true
+        atSecond defaultValue: 0, nullable: true
     }
 
 }
