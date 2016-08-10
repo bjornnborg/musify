d46dad68b8bc9d21bc5a9b78b23c105378a39e41
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-10 11:31:48 -0300

    Raising infrastructure exception if post to bigqueue fails

---
+++ canonic/src/groovy/meli/musify/canonic/exception/InfrastructureException.groovy
@@ -0,0 +1,8 @@
+package meli.musify.canonic.exception
+
+class InfrastructureException extends RuntimeException {
+
+    def InfrastructureException(msg, cause) {
+        super(msg, cause)
+    }
+}
