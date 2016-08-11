dc234c21d784e1e378843c6a1cb1eddac0d65ca4
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-11 18:24:12 -0300

    testing song stringfication

--- consumer/test/unit/meli/musify/consumer/utils/RedisUtilsSpec.groovy
+++ consumer/test/unit/meli/musify/consumer/utils/RedisUtilsSpec.groovy
@@ -2,6 +2,7 @@ package meli.musify.consumer.utils
 
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
+import meli.musify.canonic.Song
 import spock.lang.Specification
 
 @TestMixin(GrailsUnitTestMixin)
@@ -20,5 +21,15 @@ class RedisUtilsSpec extends Specification {
             def date_format = RedisUtils.MONTHLY_PLAY_COUNT_RANKING_KEY_FORMAT
             def suffix = date_format.format(new Date())
             key_name == "songs:monthly:playCount:" + suffix
+    }
+
+    void "must format a song for writing"() {
+        given: "a source song"
+            def song = new Song(name: "Da Song", album: "Album")
+            song.id = 1
+        when: "we ask for the formatting"
+            def serialization = RedisUtils.getSongRecord(song)
+        then: "serialization must match"
+            serialization == "1;Da Song - Album"
     }
 }
