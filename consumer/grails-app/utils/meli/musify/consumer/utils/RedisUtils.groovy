8f22d1f17b4823c83440249e1685e2e7d8e4d8cf
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-12 09:37:24 -0300

    Refatoring to use RedisUtils

--- consumer/grails-app/utils/meli/musify/consumer/utils/RedisUtils.groovy
+++ consumer/grails-app/utils/meli/musify/consumer/utils/RedisUtils.groovy
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat
 class RedisUtils {
 
     public static final String SONG_PLAY_COUNT_KEY_PREFFIX = "songs:playCount:%s"
+    public static final String SONG_PLAYING_COUNT_KEY_PREFFIX = "songs:playingCount:%s"
     public static final String MONTHLY_PLAY_COUNT_KEY_PREFFIX = "songs:monthly:playCount:%s"
     public static final String MONTHLY_PLAYING_COUNT_KEY_PREFFIX = "songs:monthly:playingCount:%s"
     public static DateFormat MONTHLY_PLAY_COUNT_RANKING_KEY_FORMAT = new SimpleDateFormat("MM-yyyy")
@@ -14,6 +15,10 @@ class RedisUtils {
 
     static getSongPlayCountKey(song) {
         String.format(SONG_PLAY_COUNT_KEY_PREFFIX, song.id)
+    }
+
+    static getSongPlayingCountKey(song) {
+        String.format(SONG_PLAYING_COUNT_KEY_PREFFIX, song.id)
     }
 
     static getMonthlyPlayedRankingKey() {
