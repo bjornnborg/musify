dfaf24a15af4cda0226e04f4b0804876648ce062
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-12 09:24:12 -0300

    Refactoring to use RedisUtils

--- consumer/test/unit/meli/musify/consumer/utils/RedisUtilsSpec.groovy
+++ consumer/test/unit/meli/musify/consumer/utils/RedisUtilsSpec.groovy
@@ -23,6 +23,15 @@ class RedisUtilsSpec extends Specification {
             key_name == "songs:monthly:playCount:" + suffix
     }
 
+    void "must generate most playING songs ranking key"() {
+        when: "we ask for the key name"
+            def key_name = RedisUtils.getMonthlyPlayingRankingKey()
+        then: "key name and format must match"
+            def date_format = RedisUtils.MONTHLY_PLAYING_COUNT_RANKING_KEY_FORMAT
+            def suffix = date_format.format(new Date())
+            key_name == "songs:monthly:playingCount:" + suffix
+    }
+
     void "must format a song for writing"() {
         given: "a source song"
             def song = new Song(name: "Da Song", album: "Album")
