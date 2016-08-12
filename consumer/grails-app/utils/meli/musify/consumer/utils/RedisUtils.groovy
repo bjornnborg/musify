9fc2f65ee840da7774ed976c91b3b2a328b57669
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-12 09:24:12 -0300

    Refactoring to use RedisUtils

--- consumer/grails-app/utils/meli/musify/consumer/utils/RedisUtils.groovy
+++ consumer/grails-app/utils/meli/musify/consumer/utils/RedisUtils.groovy
@@ -6,11 +6,17 @@ import java.text.SimpleDateFormat
 class RedisUtils {
 
     public static final String MONTHLY_PLAY_COUNT_KEY_PREFFIX = "songs:monthly:playCount:%s"
+    public static final String MONTHLY_PLAYING_COUNT_KEY_PREFFIX = "songs:monthly:playingCount:%s"
     public static DateFormat MONTHLY_PLAY_COUNT_RANKING_KEY_FORMAT = new SimpleDateFormat("MM-yyyy")
+    public static DateFormat MONTHLY_PLAYING_COUNT_RANKING_KEY_FORMAT = new SimpleDateFormat("MM-yyyy")
     public static final String SONG_RECORD_FORMAT = "%s;%s - %s"
 
     static getMonthlyPlayedRankingKey() {
         String.format(MONTHLY_PLAY_COUNT_KEY_PREFFIX, MONTHLY_PLAY_COUNT_RANKING_KEY_FORMAT.format(new Date()))
+    }
+
+    static getMonthlyPlayingRankingKey() {
+        String.format(MONTHLY_PLAYING_COUNT_KEY_PREFFIX, MONTHLY_PLAYING_COUNT_RANKING_KEY_FORMAT.format(new Date()))
     }
 
     static getSongRecord(song) {
--- consumer/src/groovy/meli/musify/consumer/listener/MonthlyPlayingCountRankingStrategy.groovy
+++ consumer/src/groovy/meli/musify/consumer/listener/MonthlyPlayingCountRankingStrategy.groovy
@@ -1,15 +1,13 @@
 package meli.musify.consumer.listener
 
 import grails.plugin.redis.RedisService
+import meli.musify.consumer.utils.RedisUtils
 import redis.clients.jedis.Jedis
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 
 class MonthlyPlayingCountRankingStrategy implements SongEventListener {
-
-    DateFormat MONTHLY_PLAYING_COUNT_RANKING_KEY_FORMAT = new SimpleDateFormat("MM-yyyy")
-    String SONG_RECORD_FORMAT = "%s;%s - %s"
 
     def redisService
 
@@ -25,8 +23,7 @@ class MonthlyPlayingCountRankingStrategy implements SongEventListener {
     @Override
     void execute(message, song) {
         redisService.withRedis {Jedis redis ->
-            String monthlyPlayingRankingKey = String.format("songs:monthly:playingCount:%s", MONTHLY_PLAYING_COUNT_RANKING_KEY_FORMAT.format(new Date()))
-            redis.zincrby(monthlyPlayingRankingKey, message.msg.increment?: 5, String.format(SONG_RECORD_FORMAT, song.id, song.name, song.album))
+            redis.zincrby(RedisUtils.getMonthlyPlayingRankingKey(), message.msg.increment?: 5, RedisUtils.getSongRecord(song))
         }
     }
 }
