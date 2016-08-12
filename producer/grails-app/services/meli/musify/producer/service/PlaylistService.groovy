e66a64cbb5fb9df238f0180050e86b61619ff5dc
Bjornn Borg <bjornn.borg@mercadolivre.com>
2016-08-12 14:42:05 -0300

    adding code documentation

--- producer/grails-app/services/meli/musify/producer/service/PlaylistService.groovy
+++ producer/grails-app/services/meli/musify/producer/service/PlaylistService.groovy
@@ -3,6 +3,10 @@ package meli.musify.producer.service
 import grails.transaction.Transactional
 import meli.musify.canonic.playlist.Playlist
 
+/**
+ * Las listas de reproducción se escriben y se leen de MongoDB.
+ * Una lista de reproducción y canciones se graban de forma anidada para facilitar la búsqueda y visualización
+ */
 @Transactional
 class PlaylistService {
 
@@ -10,6 +14,9 @@ class PlaylistService {
         Playlist.where{login =~ loginParam}.list()
     }
 
+    /**
+     * Graba una lista de reproducción completa, ya con canciones, para un usuario particular
+     */
     def create(playlist) {
         playlist.save(true)
     }
--- producer/grails-app/services/meli/musify/producer/service/UserService.groovy
+++ producer/grails-app/services/meli/musify/producer/service/UserService.groovy
@@ -3,6 +3,10 @@ package meli.musify.producer.service
 import grails.transaction.Transactional
 import meli.musify.canonic.User
 
+/**
+ * Los usuarios se escriben y se leen de MySQL.
+ * Objetos de dominio se mantienen en una base de datos relacional.
+ */
 @Transactional
 class UserService {
 
